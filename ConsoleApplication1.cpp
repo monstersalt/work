b025bc4d3b9abc882d6400d96961a6a255513a83
monstersalt <125230468+monstersalt@users.noreply.github.com>
2023-02-18 10:37:27 +0800

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    float fnum1 = 5.2;
+    short int a0 = 10;
+    int a = 10;
+    cout << "size of a0 is=" << sizeof(a0) << endl;
+    cout << "number1=" << a << endl;
+    cout << "e" << endl;
+}
